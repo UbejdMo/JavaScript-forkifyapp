1900babe09bf1c05ea59fa8b8b8c845b6b65291e
Ubejd Morina <morinaubejd@gmail.com>
2026-05-29 14:33:39 +0200

    add render recipe via View and recipeView

--- forkify/src/js/controller.js
+++ forkify/src/js/controller.js
@@ -1,14 +1,25 @@
 import * as model from './model.js';
+import recipeView from './views/recipeView.js';
 
 const controlRecipes = async function () {
   try {
     const id = window.location.hash.slice(1);
     if (!id) return;
 
+    // 1) Show a loading spinner immediately
+    recipeView.renderSpinner();
+
+    // 2) Load the recipe (async)
     await model.loadRecipe(id);
+
+    // 3) Render it
+    recipeView.render(model.state.recipe);
   } catch (err) {
-    console.error(err);
+    recipeView.renderError();
   }
 };
 
-['hashchange', 'load'].forEach(ev => window.addEventListener(ev, controlRecipes));
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+};
+init();
